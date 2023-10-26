2aeeee539aa465300ac583a4593ff75fc586e945
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2023-10-26 23:48:37 +0545

    commit

--- components/Ads/Popunder_Mob.js
+++ components/Ads/Popunder_Mob.js
@@ -1,41 +1,14 @@
 import Script from "next/script";
-import { useEffect } from "react";
 
 function PopunderAds() {
 
-    useEffect(() => {
-    }, []);
 
     return (
         <div className="flex items-center justify-center">
 
 
-            <Script
-                id="popunder"
-                strategy="beforeInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                    var ad_idzone = "5063202",
-                    ad_popup_fallback = true,
-                    ad_popup_force = false,
-                    ad_chrome_enabled = true,
-                    ad_new_tab = true,
-                    ad_frequency_period = 5,
-                    ad_frequency_count = 3,
-                    ad_trigger_method = 3,
-                    ad_trigger_delay = 0; 
-  `,
-                }}
-            />
 
-
-            <Script
-                strategy="afterInteractive"
-                src="https://a.pemsrv.com/popunder1000.js"
-            />
-
-
-
+            <Script src="/AdsScript/PopunderScript_Mob.js"></Script>
 
 
 
