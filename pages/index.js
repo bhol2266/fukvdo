0b88785cab38c54ddc696c16de24a4e128ecbd74
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2023-11-13 00:03:30 +0545

    commit

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,9 @@ import Sidebar from '../components/Sidebar';
 import Videos from '../components/Videos';
 import React from 'react'
 import videosContext from '../context/videos/videosContext'
-import fetchdata from 'node-fetch';
-import cheerio from 'cheerio';
 import Category_slider from '../components/category_slider';
 import HomepageTitle from '../components/HomepageTitle';
 import { getLanguge } from '../config/getLanguge';
-import { getDatabase, ref, set, get, child } from "firebase/database";
-import { app } from '../firebase';
-import Script from 'next/script';
 import { useRouter } from 'next/router';
 import { getCookie, deleteCookie, setCookie } from "cookies-next";
 import Outstreams from '../components/Ads/Outstream';
@@ -216,122 +211,17 @@ export async function getStaticProps({ req, res }) {
 
 
 
-  var finalDataArray_Arrar = []
-  var finalDataArray = []
-
-  const scrape = async (url) => {
-
-    var thumbnailArray = []
-    var TitleArray = []
-    var durationArray = []
-    var likedPercentArray = []
-    var viewsArray = []
-    var previewVideoArray = []
-    var hrefArray = []
-
-    const response = await fetchdata(url)
-    const body = await response.text();
-    const $ = cheerio.load(body)
-
-
-
-
-
-    $('.videos .video-list.video-rotate').each((i, el) => {
-
-      const select = cheerio.load(el)
-
-      select('.video-item picture img').each((i, el) => {
-
-        const data = $(el).attr("data-src")
-        thumbnailArray.push(data)
-
-
-      })
-
-      select('.video-item picture img').each((i, el) => {
-
-        const data = $(el).attr("alt")
-        TitleArray.push(data)
-
-
-      })
-
-      select('.video-item .l').each((i, el) => {
-
-        const data = $(el).text()
-        durationArray.push(data)
-      })
-
-
-
-      select('.video-item .stats').each((i, el) => {
-
-        const text = $(el).text()
-        const likePercentage = text.substring(text.indexOf("%") - 4, text.indexOf("%") + 1)
-        const views = text.substring(0, text.indexOf("%") - 4)
-
-        likedPercentArray.push(likePercentage.trim())
-        viewsArray.push(views.trim())
-
-      })
-
-
-      select('.video-item picture img').each((i, el) => {
-
-        const data = $(el).attr("data-preview")
-        previewVideoArray.push(data)
-      })
-
-
-
-      select('.video-item').each((i, el) => {
-
-        const data = $(el).children().eq(1).attr("href")
-        if (data) {
-          hrefArray.push(`https://spankbang.com${data}`)
-        }
-      })
-
-
-
-      for (let index = 0; index < thumbnailArray.length; index++) {
-
-        if (hrefArray[index] != undefined && previewVideoArray[index] != undefined && !thumbnailArray[index].includes("//assets.sb-cdate.com")) {
-
-          finalDataArray.push({
-            thumbnailArray: thumbnailArray[index],
-            TitleArray: TitleArray[index],
-            durationArray: durationArray[index],
-            likedPercentArray: likedPercentArray[index],
-            viewsArray: viewsArray[index],
-            previewVideoArray: previewVideoArray[index],
-            hrefArray: hrefArray[index],
-
-          })
-        }
-      }
-
-      if (finalDataArray.length > 2) {
-        finalDataArray_Arrar.push(finalDataArray)
-      }
-
-      thumbnailArray = []
-      TitleArray = []
-      durationArray = []
-      likedPercentArray = []
-      viewsArray = []
-      previewVideoArray = []
-      hrefArray = []
-
-      finalDataArray = []
-    })
-
-
-  }
-
-
-  await scrape(`https://spankbang.party/`)
+  const parcelData = { href: "https://spankbang.party/" }
+  const rawResponse = await fetch(`https://clownfish-app-jn7w9.ondigitalocean.app/getHomePageVideos`, {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify(parcelData),
+  });
+  const ress = await rawResponse.json();;
+  const finalDataArray_Arrar=ress.data;
 
 
   var desiVideosDataArray = []
