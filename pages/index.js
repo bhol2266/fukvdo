25ad21eb123f5e91f8eeeba9f13ffe08076b8e61
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2023-11-23 23:36:52 +0545

    sadfdsaf

--- pages/index.js
+++ pages/index.js
@@ -212,7 +212,7 @@ export async function getServerSideProps({ req, res }) {
 
 
   const parcelData = { href: "https://spankbang.party/" }
-  const rawResponse = await fetch(`https://fukvdo.vercel.app/api/spangbang/homepage`, {
+  const rawResponse = await fetch(`https://chutlunds-com-puce.vercel.app/api/spangbang/homepage`, {
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
