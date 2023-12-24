56ff5b103b809e3b71fa649559fb6c0fc32d5ed5
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2023-12-25 00:55:58 +0545

    sdfasd

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   reactStrictMode: true,
+  experimental:{
+    forceSwcTransforms:true,
+  },
   images: {
     domains: ['links.papareact.com', 'platform-lookaside.fbsbx', 'firebasestorage.googleapis.com', 'lh3.googleusercontent.com', 'avatars.githubusercontent.com', 'www.pexels.com', 'hotdesipics.co', 'i.picsum.photos', 'static-ca-cdn.eporner.com', 'imggen.eporner.com', 'www.pezporn.com', 'https://www.pornpics.de/', 'cdni.pornpics.de', 'static-sg-cdn.eporner.com']
   },
